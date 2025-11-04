de210a56780936d92ed7b71882d6c3b722896654
Felix Treykorn <felix.treykorn@student.hpi.uni-potsdam.de>
2025-11-04 15:19:00 +0100

    refactor: adds document comments to SpatialRecord.java

--- wayang-commons/wayang-basic/src/main/java/org/apache/wayang/basic/data/SpatialRecord.java
+++ wayang-commons/wayang-basic/src/main/java/org/apache/wayang/basic/data/SpatialRecord.java
@@ -3,10 +3,17 @@ package org.apache.wayang.basic.data;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.io.ParseException;
 import org.locationtech.jts.io.WKTReader;
+
 /**
  * A specialization of {@link Record} for spatial data.
  */
 public class SpatialRecord extends Record {
+    /**
+     * Retrieve a field as a {@link Geometry}. It must be castable as such.
+     *
+     * @param index the index of the field
+     * @return the {@link Geometry} representation of the field
+     */
     public Geometry getGeometry(final int index, final WKTReader reader) throws ParseException {
         final Object field = this.getValues()[index];
         if (field instanceof Geometry) {
